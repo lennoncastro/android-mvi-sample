9c78924b0d85e31d4c3c458a13f3017e67d548a7
lennoncastro <lennon.castro.92@gmail.com>
2022-07-20 21:56:07 -0300

    added: @Singleton scope to NetworkModule.kt in bindHttpClient method

--- network/src/main/java/mvisample/network/di/NetworkModule.kt
+++ network/src/main/java/mvisample/network/di/NetworkModule.kt
@@ -1,13 +1,18 @@
 package mvisample.network.di
 
+import android.app.Application
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
 import mvisample.network.di.httpClient.HttpClient
 import mvisample.network.di.httpClient.HttpClientImpl
+import javax.inject.Singleton
 
 @Module
+@InstallIn(Application::class)
 abstract class NetworkModule {
 
+    @Singleton
     @Binds
     abstract fun bindHttpClient(httpClientImpl: HttpClientImpl): HttpClient
 }
